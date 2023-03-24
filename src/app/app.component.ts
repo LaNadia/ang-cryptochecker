dd76e1db58277da22da77277b46ab96cd87c1cf9
LaNadia <setsias@gmail.com>
2023-03-24 16:57:56 +0400

    create branch first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  selectedCurrency: string = 'INR';
+  selectedCurrency: string = 'ENR';
 
   constructor(private CurrencyService: CurrencyService){}
 
